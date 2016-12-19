a4ac1d895bb31ee15357546fa36f593ed80449b8
sahana kamath <sahanakamath@sahanas-MacBook-Pro.local>
2016-12-19 18:14:05 -0500

    About Me section completed

--- src/About.js
+++ src/About.js
@@ -6,18 +6,18 @@ class About extends Component {
     return (
       <div className="About Padding-style section-margin Background-white" id="about">
       <h2 className="Text Font-header text-center">ABOUT ME</h2>
-      <div className="container">
+      <div className="container within-section-margin Text">
         <div className="row">
           <div className="col-sm-3">
-            <div className="profilePhoto img-circle"></div>
+            <div className="profilePhoto img-circle center-block"></div>
           </div>
           <div className="col-sm-9">
-              <p>I'm a Junior Web Developer, and a recent graduate from the Web Development Course at Brainstation. Prior to grad school, I worked as a digital QA, and a junior frontend developer for a consulting company.
+              <p className="Text-body-size">I'm a Junior Web Developer, and a recent graduate from the Web Development Course at Brainstation. Prior to grad school, I worked as a digital QA, and a junior frontend developer for a consulting company.
               </p>
-              <p>
+              <p className="Text-body-size">
               My goal as a web developer is to make people's interaction with technology accessible, intuitive and useful.
               </p>
-              <p>
+              <p className="Text-body-size">
               When I am not developing, you can see me experimenting with cooking, clicking photos, exploring new places and meeting friends 
               </p>
           </div>
