89d191ba5e4c2a39d241c4c7b1fc30622b5204e3
sahana kamath <sahanakamath@sahanas-MacBook-Pro.local>
2017-01-09 09:38:51 -0500

    project section

--- src/Projects.js
+++ src/Projects.js
@@ -7,20 +7,38 @@ class Projects extends Component {
     <section className="Projects Padding-style Background-azure" id="project">
         <h2 className="Text text-center Font-header">PROJECTS</h2>
 
-        <div className="Project4">
-            <div className="Padding-style Sub-text">
+        <div className="Project3 row">
+            <div className="Padding-style Sub-text col-xs-12 col-sm-9">
                 <h3>Qzam</h3>
                 <h4>Responsive quiz application.</h4>
-                <p> HTML | CSS/SASS | Bootstrap | jQuery</p>
+                <p> React JS | Bootstrap | CSS | Javascript | PostgresSQL | AWS | REST API | Responsive</p>
+            </div>
+            <div className="Padding-style Sub-text col-xs-12 col-sm-3">
+                <img className="imageSize center-block" src={require('../public/image/qzam_laptop.png')}/>
             </div>
         </div>
 
-         <div className="row Project-1 container section-margin">
-            <h3>PSD to Responsive Site</h3>
-            <h4>Converting existing PSD design to fully responsive web page.</h4>
-            <p> HTML | CSS/SASS | Bootstrap | jQuery</p>
+         <div className="Project2 row Background-black">
+            <div className="Padding-style Sub-text col-xs-12 col-sm-9">
+                <h3>Weatherz</h3>
+                <h4>Responsive Weather application.</h4>
+                <p> React JS | Bootstrap | CSS | Google Weather API | Responsive</p>
+            </div>
+            <div className="Padding-style Sub-text col-xs-12 col-sm-3">
+                <img className="imageSize center-block" src={require('../public/image/weatherApp.png')}/>
+            </div>
         </div>
 
+        <div className="Project1 row">
+            <div className="Padding-style Sub-text col-xs-12 col-sm-9">
+                <h3>Jukebox</h3>
+                <h4>Responsive Music application.</h4>
+                <p> Bootstrap | SASS / CSS | Responsive | jQuery</p>
+            </div>
+            <div className="Padding-style Sub-text col-xs-12 col-sm-3">
+                <img className="imageSize center-block" src={require('../public/image/ipad.png')}/>
+            </div>
+        </div>
     </section>
     );
   }
